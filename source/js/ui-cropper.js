a56835f556123d07d3cc77054241298c1e3ac3ba
emanuel douglas <emanuel.douglas.sc@gmail.com>
2019-08-06 16:40:00 -0300

    Adicioan a opção de forçar a imagem atualizar mesmo sendo a mesma imagem cortada.

--- source/js/ui-cropper.js
+++ source/js/ui-cropper.js
@@ -81,7 +81,7 @@ angular.module('uiCropper').directive('uiCropper', ['$timeout', 'cropHost', 'cro
                 scope.areaCoords = cropHost.getAreaCoords();
             };
 
-            var updateResultImage = function (scope, force, callback) {
+            var updateResultImage = function (scope, force, callback, forceImageUpdate) {
                 if (scope.image !== '' && (!scope.liveView.block || force)) {
                     var resultImageObj = cropHost.getResultImage();
                     var resultImage;
@@ -93,7 +93,7 @@ angular.module('uiCropper').directive('uiCropper', ['$timeout', 'cropHost', 'cro
                     }
 
                     var urlCreator = window.URL || window.webkitURL;
-                    if (storedResultImage !== resultImage) {
+                    if (forceImageUpdate || (storedResultImage !== resultImage)) {
                         storedResultImage = resultImage;
                         scope.resultImage = resultImage;
                         if (scope.liveView.callback) {
@@ -125,8 +125,8 @@ angular.module('uiCropper').directive('uiCropper', ['$timeout', 'cropHost', 'cro
             };
 
             if (scope.liveView && typeof scope.liveView.block === 'boolean') {
-                scope.liveView.render = function (callback) {
-                    updateResultImage(scope, true, callback);
+                scope.liveView.render = function (callback, forceImageUpdate) {
+                    updateResultImage(scope, true, callback, forceImageUpdate);
                 };
             } else {
                 scope.liveView = {block: false};
